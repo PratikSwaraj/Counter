995c062c77555fd99b86c1742a2840ecb296ed1c
PratikSwaraj <147915251+PratikSwaraj@users.noreply.github.com>
2024-02-01 02:17:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import "./styles.css";
+
+// export default function App() {
+//   const [value, setValue] = useState(0);
+//   const increase = () => {
+//     setValue((prev) => prev + 1);
+//   };
+//   const decrease = () => {
+//     setValue((prev) => prev - 1);
+//   };
+//   return (
+//     <div className="App">
+//       <h1>Counter App</h1>
+//       <p>Count: {value}</p>
+//       <button onClick={increase}>Increment</button>
+//       <button onClick={decrease}>Decrement</button>
+//     </div>
+//   );
+// }
+
+import React, { Component } from "react";
 import "./styles.css";
 
-export default function App() {
-  const [value, setValue] = useState(0);
-  const increase = () => {
-    setValue((prev) => prev + 1);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+  }
+
+  increase = () => {
+    this.setState((prevState) => ({
+      value: prevState.value + 1
+    }));
   };
-  const decrease = () => {
-    setValue((prev) => prev - 1);
+
+  decrease = () => {
+    this.setState((prevState) => ({
+      value: prevState.value - 1
+    }));
   };
-  return (
-    <div className="App">
-      <h1>Counter App</h1>
-      <p>Count: {value}</p>
-      <button onClick={increase}>Increment</button>
-      <button onClick={decrease}>Decrement</button>
-    </div>
-  );
+
+  render() {
+    const { value } = this.state;
+    return (
+      <div className="App">
+        <h1>Counter App</h1>
+        <p>Count: {value}</p>
+        <button onClick={this.increase}>Increment</button>
+        <button onClick={this.decrease}>Decrement</button>
+      </div>
+    );
+  }
 }
+
+export default App;
